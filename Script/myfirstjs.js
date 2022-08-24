dd32539ead554c8263ecc756a7e8b9528e2f388c
gooddeath333 <ilya.deusus91@mail.ru>
2022-08-24 23:21:27 +0300

    домашка

---
+++ Script/myfirstjs.js
@@ -0,0 +1,20 @@
+// function myFunction() {
+// alert("Вы помацали")
+// }
+
+// let departure = "Moscow";
+// let arrival = "Paris";
+
+
+// function myFunction(departure, arrival, result) {
+// 	console.log(`From ${departure} to ${arrival}`);
+// 	alert(`From ${departure} to ${arrival}`);
+
+// }
+
+// let result = prompt("How old are you?")
+// console.log(result);
+
+
+
+// console.log("Hello, my friends!!!")
